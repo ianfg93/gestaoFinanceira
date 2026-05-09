1263e38f1a50b5063817150941c6a98be477e2b8
Ian Ferreira <ian_ferreira93@hotmail.com>
2026-05-09 14:26:00 -0300

    libera CORS para FRONTEND_URL em producao

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,10 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => array_values(array_filter(array_unique([
         'http://localhost:5173',
         'http://localhost:3000',
-    ],
+        rtrim((string) env('FRONTEND_URL', ''), '/'),
+    ]))),
 
     'allowed_origins_patterns' => [],
 
