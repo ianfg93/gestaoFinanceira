75d839946deb1897eaab46fd39a9f80afc480ae2
Ian Ferreira <ian_ferreira93@hotmail.com>
2026-05-11 12:27:29 -0300

    forca registro manual de rotas api/web no bootstrap

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,17 +5,21 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        apiPrefix: '',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        using: function () {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(HandleCors::class);
